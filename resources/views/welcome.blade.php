367f01f41564e8d28e04d21867fdbe7a4fa57861
vovan4a86 <vovan4a86@mail.ru>
2025-09-09 15:25:34 +0300

    Sponsors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
                         </svg>
 
                         <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                            Sponsor
+                            Sponsors
                         </a>
                     </div>
                 </div>
